e2c479adc59740870dd1b4cfbce634a222f0d1ae
dinhvan <dinhvanle9xpro@gmail.com>
2026-07-17 11:24:15 +0700

    add file

---
+++ promise-all.js
@@ -0,0 +1,31 @@
+function myPromiseAll(promises) {
+    return new Promise((resolve, reject) => {
+
+        const results = [];
+        let completed = 0;
+
+        if (promises.length === 0) {
+            resolve([]);
+            return;
+        }
+
+        promises.forEach((promise, index) => {
+
+            Promise.resolve(promise)
+                .then(value => {
+
+                    results[index] = value;
+
+                    completed++;
+
+                    if (completed === promises.length) {
+                        resolve(results);
+                    }
+
+                })
+                .catch(reject);
+
+        });
+
+    });
+}
