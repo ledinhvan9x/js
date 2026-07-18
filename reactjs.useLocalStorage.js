74f96ede4c98e9c3c7f49c68e78a6770f7abd402
dinhvan <dinhvanle9xpro@gmail.com>
2026-07-18 14:03:32 +0700

    reactjs localstorage custom hook

---
+++ reactjs.useLocalStorage.js
@@ -0,0 +1,12 @@
+function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored ? JSON.parse(stored) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
