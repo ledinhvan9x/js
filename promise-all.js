3a67433b54da8f09f879f53598b6d5819be41665
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-08-15 10:33:48 +0700

    Update promise-all.js

--- promise-all.js
+++ promise-all.js
@@ -8,24 +8,16 @@ function myPromiseAll(promises) {
             resolve([]);
             return;
         }
-
         promises.forEach((promise, index) => {
-
             Promise.resolve(promise)
                 .then(value => {
-
                     results[index] = value;
-
                     completed++;
-
                     if (completed === promises.length) {
                         resolve(results);
                     }
-
                 })
                 .catch(reject);
-
         });
-
     });
 }
