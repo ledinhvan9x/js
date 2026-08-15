4ec9b10d78873dc16c9cbcc161c05b98f9032668
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-08-15 10:33:15 +0700

    Update promise-all-settled.js

--- promise-all-settled.js
+++ promise-all-settled.js
@@ -1,6 +1,5 @@
 function myPromiseAllSettled(promises) {
     return new Promise(resolve => {
-
         if (promises.length === 0) {
             resolve([]);
             return;
@@ -10,35 +9,25 @@ function myPromiseAllSettled(promises) {
         let completed = 0;
 
         promises.forEach((promise, index) => {
-
             Promise.resolve(promise)
                 .then(value => {
-
                     results[index] = {
                         status: "fulfilled",
                         value
                     };
-
                 })
                 .catch(reason => {
-
                     results[index] = {
                         status: "rejected",
                         reason
                     };
-
                 })
                 .finally(() => {
-
                     completed++;
-
                     if (completed === promises.length) {
                         resolve(results);
                     }
-
                 });
-
         });
-
     });
 }
