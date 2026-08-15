a0fcfd5866852a3e10b68182c2fb9712562407a7
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-08-15 10:34:42 +0700

    Update promise-race.js

--- promise-race.js
+++ promise-race.js
@@ -1,13 +1,9 @@
 function myPromiseRace(promises) {
     return new Promise((resolve, reject) => {
-
         promises.forEach(promise => {
-
             Promise.resolve(promise)
                 .then(resolve)
                 .catch(reject);
-
         });
-
     });
 }
